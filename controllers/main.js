59ea2bf5e5ee434acbf86083befe7ec078f3190b
Yeva Yu <yevayu@hotmail.com>
2017-01-21 11:59:10 -0500

    Heyy

--- controllers/main.js
+++ controllers/main.js
@@ -4,13 +4,17 @@ var router = express.Router();
 var myDb = require('../db');
 var dbname = "Law";
 
+route.get('/', funtion(req, res) {
+  res.send("Hello!");
+});
+
 router.get('/data/', function(req, res) {
   console.log(req.query);
   var categories = req.query.keywords;
   if (typeof categories === 'string') {
     var temp = categories
     categories = [];
-    categories.push(temp); 
+    categories.push(temp);
     console.log(categories);
   }
   var collection = myDb.get().collection(dbname);
