e930b527f776fcf4b516b83ad0f58f60a52a0444
Yeva Yu <yevayu@hotmail.com>
2017-05-26 02:51:38 -0400

    update ports

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var db = require('./db');
 var app = express();
 
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/', require('./controllers/main'));
 
 var url = 'mongodb://yevayu:yaj3ecbG1aOpwYsjKl5p7vtT0S72RnzDK5xcTctj12PD1gXrx8I2zUYwEVwghjntalGzxkhUvBhF9lk9xOlwyA==@yevayu.documents.azure.com:10255/?ssl=true';
@@ -11,8 +14,8 @@ db.connect(url, function(err, db) {
     console.log('Error connecting to server');
     process.exit(1);
   } else {
-    app.listen(3000, function() {
-      console.log("listening on port 3000");
+    app.listen(app.get('port'), function() {
+      console.log('Node app is running on port', app.get('port'));
     });
   }
 });
